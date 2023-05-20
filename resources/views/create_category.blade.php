c20e06d4ad5039e57738b1f1045c52cf67f27f50
Ryudenshi <skarydog17@gmail.com>
2023-05-20 23:18:27 +0300

    poster/category finished menage

--- resources/views/create_category.blade.php
+++ resources/views/create_category.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 
 <div class="container">
+    <h3>Create category</h3>
+
     <form action="/fuukaProject/public/api/V1/categories" method="post" enctype="multipart/form-data">
         @csrf
 
@@ -17,10 +19,78 @@
         </div>
 
         <div class="form-group">
-            <button type="submit" class="btn btn-primary">Add Category</button>
+            <button type="submit" class="btn btn-success">Add Category</button>
         </div>
 
     </form>
-</div>
 
+    <hr class="my-4">
+
+    <div class="row">
+        <h3 class="mx-auto">Category list</h3>
+    </div>
+
+    <div class="row">
+        @foreach($categories as $category)
+        <div class="col-4 mb-5">
+            <div class="container">
+                <div class="card p-2">
+                    <div class="row">
+                        <div class="col-7">
+                            <h4 class="ml-3 mt-4">{{ $category->name }}</h4>
+                        </div>
+
+                        <div class="col-3">
+                            <form action="/fuukaProject/public/api/V1/categories/{{ $category->id }}" method="post" id="deleteCategoryForm">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete category</button>
+                            </form>
+                            <button type="button" style="width: 130px;" class="btn btn-primary mt-2" data-toggle="modal" data-target="#updateCategoryModal{{ $category->id }}">Update category</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- Update Category Modal -->
+        <div class="modal fade" id="updateCategoryModal{{ $category->id }}" tabindex="-1" role="dialog" aria-labelledby="updateCategoryModalLabel{{ $category->id }}" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="updateCategoryModalLabel{{ $category->id }}">Update Category</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="/fuukaProject/public/api/V1/categories/{{ $category->id }}" method="post" enctype="multipart/form-data" id="updateCategoryForm{{ $category->id }}">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group">
+                                <label for="name">Name:</label>
+                                <input type="text" class="form-control" name="name" id="name{{ $category->id }}" value="{{ $category->name }}" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="description">Description:</label>
+                                <textarea class="form-control" name="description" id="description{{ $category->id }}" required>{{ $category->description }}</textarea>
+                            </div>
+
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-success">Update</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Update Category Modal -->
+
+        @endforeach
+    </div>
+</div>
 @endsection
--- resources/views/create_poster.blade.php
+++ resources/views/create_poster.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
 
-    <div class="row border-bottom">
+    <div class="row">
 
         <h3>Create poster</h3>
 
@@ -45,106 +45,102 @@
             </div>
 
             <div class="form-group">
-                <button type="submit" class="btn btn-primary">Add Poster</button>
+                <button type="submit" class="btn btn-success">Add Poster</button>
             </div>
         </form>
     </div>
 
-    <br>
+    <hr class="mt-4 mb-2">
 
-    <div class="row  border-bottom p-4">
+    <div class="row">
 
-        <div class="col-6">
-
+        <div class="row my-4">
             <h3>Delete poster</h3>
+        </div>
 
-            @foreach($posters as $poster)
+        @foreach($posters as $poster)
+        <div class="col-4 mb-5">
             <div class="container">
-                <div class="card">
+                <div class="card p-2" style="max-height: 260px;">
                     <div>
                         <h4 class="ml-3 mt-2">{{ $poster->title }}</h4>
                     </div>
                     <div class="row">
-                        <div class="col-6">
-                            <img class="mb-3" style="width: 50%" src="{{ asset('storage/' . $poster->image_url) }}" alt="Poster Image">
+                        <div class="col-6" style="max-height: 200px;">
+                            <img class="mb-3" style="width: 100%" src="{{ asset('storage/' . $poster->image_url) }}" alt="Poster Image">
                         </div>
+                        <div class="col-1"></div>
                         <div class="col-3">
                             <form action="/fuukaProject/public/api/V1/posters/{{ $poster->id }}" method="post" id="deletePosterForm">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete Poster</button>
+                                <button type="submit" style="width: 120px;" class="btn btn-danger">Delete Poster</button>
                             </form>
+                            <button type="button" style="width: 120px;" class="btn btn-primary mt-2" data-toggle="modal" data-target="#updatePosterModal{{ $poster->id }}">Update poster</button>
                         </div>
                     </div>
                 </div>
             </div>
-            @endforeach
-
         </div>
-
-        <div class="col-6">
-
-            <h3>Update poster</h3>
-
-            @foreach($posters as $poster)
-            <div class="container">
-                <div class="card">
-                    <div>
-                        <h4 class="ml-3 mt-2">{{ $poster->title }}</h4>
+        <div class="modal fade" id="updatePosterModal{{ $poster->id }}" tabindex="-1" role="dialog" aria-labelledby="updatePosterModalLabel{{ $poster->id }}" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="updatePosterModalLabel{{ $poster->id }}">Update Poster</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                    <div class="row">
+                    <div class="modal-body">
                         <div class="col-6">
-                            <img class="mb-3" style="width: 50%" src="{{ asset('storage/' . $poster->image_url) }}" alt="Poster Image">
+                            <img class="mb-3" style="width: 100%" src="{{ asset('storage/' . $poster->image_url) }}" alt="Poster Image">
                         </div>
-                        <div class="col-3">
-                            <form action="/fuukaProject/public/api/V1/posters/{{ $poster->id }}" method="post" enctype="multipart/form-data" id="updatePoster">
-                                @csrf
-                                @method('PUT')
+                        <form action="/fuukaProject/public/api/V1/posters/{{ $poster->id }}" method="post" enctype="multipart/form-data" id="updatePoster">
+                            @csrf
+                            @method('PUT')
 
-                                <p>{{ $poster->id }}</p>
+                            <p>{{ $poster->id }}</p>
 
-                                <div class="form-group">
-                                    <label for="title">Title:</label>
-                                    <input type="text" class="form-control" name="title" id="title" required>
-                                </div>
+                            <div class="form-group">
+                                <label for="title">Title:</label>
+                                <input type="text" class="form-control" name="title" id="title" required>
+                            </div>
 
-                                <div class="form-group">
-                                    <label for="description">Description:</label>
-                                    <textarea class="form-control" name="description" id="description" required></textarea>
-                                </div>
+                            <div class="form-group">
+                                <label for="description">Description:</label>
+                                <textarea class="form-control" name="description" id="description" required></textarea>
+                            </div>
 
-                                <div class="form-group">
-                                    <label for="image_url">Image:</label>
-                                    <input type="file" class="form-control-file" name="image_url" id="image_url" accept="image/*" required>
-                                </div>
+                            <div class="form-group">
+                                <label for="image_url">Image:</label>
+                                <input type="file" class="form-control-file" name="image_url" id="image_url" accept="image/*" required>
+                            </div>
 
-                                <div class="form-group">
-                                    <label for="price">Price:</label>
-                                    <input type="number" class="form-control" name="price" id="price" required>
-                                </div>
+                            <div class="form-group">
+                                <label for="price">Price:</label>
+                                <input type="number" class="form-control" name="price" id="price" required>
+                            </div>
 
-                                <div class="form-group">
-                                    <label for="categories">Categories:</label>
-                                    <select class="form-control" name="categories[]" id="categories" multiple required>
-                                        @foreach($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
+                            <div class="form-group">
+                                <label for="categories">Categories:</label>
+                                <select class="form-control" name="categories[]" id="categories" multiple required>
+                                    @foreach($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary">Update</button>
-                                </div>
-                            </form>
-                        </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-success">Update</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
-
-
 
 </div>
 
